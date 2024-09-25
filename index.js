214ffbdd44b8e0f8f247b3e65d3ace625cadedf0
NaimoonJannat <naimoonjannat@gmail.com>
2024-09-25 15:14:29 +0600

    Server ready for running

--- index.js
+++ index.js
@@ -22,3 +22,26 @@ const client = new MongoClient(uri, {
       deprecationErrors: true,
     }
   });
+
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+    //   await client.connect();
+
+
+    
+      // // Send a ping to confirm a successful connection
+      // await client.db("admin").command({ ping: 1 });
+      // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+      //   await client.close();
+      }
+    }
+    run().catch(console.log);
+    app.get('/', (req, res) => {
+      res.send('Auraloom Server Running')
+    })
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
